ea702c6ed2f2cb74be6651cc4de158196c09ccbb
John <jolivaresp1700@alumno.ipn.mx>
2023-06-15 16:04:14 -0600

    Commit PayPal 1.1

--- app/src/main/java/com/myaplication/policarrito/PagosActivity.kt
+++ app/src/main/java/com/myaplication/policarrito/PagosActivity.kt
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.paypal.checkout.PayPalCheckout
 import com.paypal.checkout.config.CheckoutConfig
+import com.paypal.checkout.config.Environment
 import com.paypal.checkout.config.SettingsConfig
 import com.paypal.checkout.createorder.CurrencyCode
 import com.paypal.checkout.createorder.UserAction
+import com.paypal.pyplcheckout.BuildConfig
 
 class PagosActivity : AppCompatActivity() {
+
+
+    paypalbutton Btn_Paypal
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagos)
 
 
+        val YOUR_CLIENT_ID = ""
+
         val config = CheckoutConfig(
-            application = this,
+            application = application,
             clientId = YOUR_CLIENT_ID,
             environment = Environment.SANDBOX,
-            returnUrl = "${BuildConfig.APPLICATION_ID}://paypalpay",
+            returnUrl = "${"com.myaplication.policarrito"}://paypalpay",
             currencyCode = CurrencyCode.USD,
             userAction = UserAction.PAY_NOW,
             settingsConfig = SettingsConfig(
