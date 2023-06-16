bbadb27372ad63dcd5ce182550d5b446b652c1a6
John <jolivaresp1700@alumno.ipn.mx>
2023-06-15 21:17:55 -0600

    Commit PayPal 1.1

--- app/src/main/java/com/myaplication/policarrito/PagosActivity.kt
+++ app/src/main/java/com/myaplication/policarrito/PagosActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 
+
 class PagosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
