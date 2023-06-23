5637b7b5f51bcd1965ef05f17b1b08bdedc68587
GALLARDO RODRÍGUEZ ENRIQUE <egallardoe1800@gmail.com>
2023-06-22 20:27:34 -0600

    Version Datos 1.1

--- app/src/main/java/com/myaplication/policarrito/ProductosActivity.kt
+++ app/src/main/java/com/myaplication/policarrito/ProductosActivity.kt
@@ -1,11 +1,14 @@
 package com.myaplication.policarrito
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
@@ -14,25 +17,57 @@ class ProductosActivity : AppCompatActivity() {
 
     //
 
-
-    val query = FirebaseDatabase.getInstance().reference.child("publicaciones")
-
-    /*
-    private val query = FirebaseDatabase.getInstance("https://smart-cart-41df4-default-rtdb.firebaseio.com/")
-        .reference.child("Frutas")
-
-     */
+    private lateinit var dbref : DatabaseReference
+    private lateinit var userRecyclerview : RecyclerView
+    private lateinit var userArrayList : ArrayList<Productos>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_productos)
 
+        userRecyclerview = findViewById(R.id.ReVi_Datos)
+        userRecyclerview.layoutManager = LinearLayoutManager(this)
+        userRecyclerview.setHasFixedSize(true)
+
+        userArrayList = arrayListOf<Productos>()
+        getUserData()
+
+    }
+
+    private fun getUserData() {
+
+        dbref = FirebaseDatabase.getInstance().getReference("Frutas")
+
+        dbref.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()){
+
+                    for (userSnapshot in snapshot.children){
 
 
+                        val user = userSnapshot.getValue(Productos::class.java)
+                        userArrayList.add(user!!)
+
+                    }
+
+                    userRecyclerview.adapter = MyAdapter(userArrayList)
+
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
 
     }
 }
-
 
 
