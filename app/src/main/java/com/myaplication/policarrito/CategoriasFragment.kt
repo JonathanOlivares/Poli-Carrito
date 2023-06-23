6cf88842f36c72edca10818af2eb091046f3a9e7
GALLARDO RODRÍGUEZ ENRIQUE <egallardoe1800@gmail.com>
2023-06-22 20:29:54 -0600

    Version Datos 1.2

--- app/src/main/java/com/myaplication/policarrito/CategoriasFragment.kt
+++ app/src/main/java/com/myaplication/policarrito/CategoriasFragment.kt
@@ -1,10 +1,14 @@
 package com.myaplication.policarrito
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +33,27 @@ class CategoriasFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categorias, container, false)
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View?
+    {
+        /* - - - - - Agregado - Informacion Correo - - - - - */
+        val view: View = inflater.inflate(R.layout.fragment_categorias, container, false)
+
+
+        val BotonSignup = view.findViewById<TextView>(R.id.Btn_Frutas)
+
+        BotonSignup.setOnClickListener{
+
+            activity?.let{
+                val win_login = Intent (it, ProductosActivity::class.java)
+                it.startActivity(win_login)
+            }
+        }
+
+
+        return view
     }
 
     companion object {
