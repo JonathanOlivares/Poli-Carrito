e71a8dbd14ac20dae387f94eb7debea9a8c68865
John <jolivaresp1700@alumno.ipn.mx>
2023-06-26 23:43:19 -0600

    Commit PayPal 1.1

--- app/src/main/java/com/myaplication/policarrito/CategoriasFragment.kt
+++ app/src/main/java/com/myaplication/policarrito/CategoriasFragment.kt
@@ -46,7 +46,7 @@ class CategoriasFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_categorias, container, false)
 
 
-        val BotonSignup = view.findViewById<TextView>(R.id.Btn_Frutas)
+        val BotonSignup = view.findViewById<TextView>(R.id.TeVi_Frutas)
         val BotonCamara = view.findViewById<TextView>(R.id.Btn_Camara)
 
         BotonSignup.setOnClickListener{
